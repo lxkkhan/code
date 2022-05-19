108c3327c92becdc33adace7e4b6679d3843d0f5
刘洺沅 <17602264257@163.com>
2022-05-19 21:54:37 +0800

    "数据步骤驱动——yaml配置测试步骤"

--- test_appium/page/base_page.py
+++ test_appium/page/base_page.py
@@ -1,5 +1,7 @@
+import yaml
 from appium.webdriver.common.mobileby import MobileBy
 from appium.webdriver.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 
 class BasePage():
@@ -7,5 +9,19 @@ class BasePage():
     def __init__(self, diver:WebDriver=None):
         self._driver = diver
     def id(self, value):
-        return self._driver.find_element(MobileBy.ID, value)
-    
+        return self._driver.find_element(By.ID, value)
+
+    def find(self, locator, value):
+        return self._driver.find_element(locator, value)
+
+    def steps(self,path):
+        with open(path) as f:
+            steps = yaml.safe_load(f)
+        element = None
+        for step in steps:
+            if "by" in step.keys():
+                element = self.find(step['by'], step['locator'])
+            if "action" in step.keys():
+                action =step["action"]
+                if action == "click":
+                    element.click()
--- test_appium/page/main.py
+++ test_appium/page/main.py
@@ -3,5 +3,5 @@ from test_appium.page.base_page import BasePage
 
 class Main(BasePage):
     def go_to_search(self):
-        self.id("tv_search").click()
-        
+        # self.id("tv_search").click()
+        self.steps("../page/main.yaml")
